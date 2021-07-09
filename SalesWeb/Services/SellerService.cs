b00974bb75cbaf7f74e8ae5424377770beabb206
luise-rios <luisemiranda@hmail.com>
2021-07-09 13:43:36 -0300

    simple create form

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -18,6 +18,12 @@ namespace SalesWeb.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 
 }
